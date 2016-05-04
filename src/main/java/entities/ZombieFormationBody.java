b7a422188322b3ff42f69fb4e63193e396b587c7
Alison Tsai <alison_tsai@brown.edu>
2016-05-04 14:23:50 -0400

    zombie formation files

---
+++ src/main/java/entities/ZombieFormationBody.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.Map;
+
+public class ZombieFormationBody extends Zombie {
+
+  public ZombieFormationBody(Entity other, Map<String, Player> players) {
+    super(other, players);
+    // TODO Auto-generated constructor stub
+  }
+
+}
---
+++ src/main/java/entities/ZombieFormationHead.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.Map;
+
+public class ZombieFormationHead extends Zombie {
+
+  public ZombieFormationHead(Entity other, Map<String, Player> players) {
+    super(other, players);
+    // TODO Auto-generated constructor stub
+  }
+
+}
