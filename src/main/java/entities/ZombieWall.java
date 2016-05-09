3bbbbe0e7d5d62733a077f9743cad713e87ff2be
blulub <boning_lu@brown.edu>
2016-05-09 00:01:43 -0400

    Zombie wall now has gap for player

--- src/main/java/entities/ZombieWall.java
+++ src/main/java/entities/ZombieWall.java
@@ -3,6 +3,7 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import edu.brown.cs.altsai.game.Window;
 
@@ -23,18 +24,50 @@ public class ZombieWall {
     List<String> zfbIds = new ArrayList<>();
     int n = 1;
 
+    Random r = new Random();
+    int gap = r.nextInt(13);
+
     while (!atCap) {
-      ZombieFormationBody b1 = new ZombieFormationBody(z, players, n, 270.0);
-      zombies.put(b1.getID(), b1);
-      zfbIds.add(b1.getID());
 
-      ZombieFormationBody b2 = new ZombieFormationBody(z, players, n, 90.0);
-      zombies.put(b2.getID(), b2);
-      zfbIds.add(b2.getID());
+      // if we are at the randomly generated gap, skip the next 4 updates
+      if (n == gap) {
+        // pick a random side to fill in (so the other side gets the gap)
+        if (r.nextInt(2) == 0) {
+          while (Math.abs(n - gap) <= 3) {
+            ZombieFormationBody b1 = new ZombieFormationBody(z, players, n, 270.0);
+            zombies.put(b1.getID(), b1);
+            zfbIds.add(b1.getID());
+            n++;
+            yPos -= 20;
+            atCap = yPos < 60;
+          }
+        } else {
+          while (Math.abs(n - gap) <= 2) {
+            n++;
+            yPos -= 20;
+            atCap = yPos < 60;
+            ZombieFormationBody b2 = new ZombieFormationBody(z, players, n, 90.0);
+            if (b2.getY() < Window.height - 30) {
+            zombies.put(b2.getID(), b2);
+            zfbIds.add(b2.getID());
+            }
+          }
+        }
+      } else {
 
-      yPos -= 20;
-      atCap = yPos < 60;
-      n += 1;
+        ZombieFormationBody b1 = new ZombieFormationBody(z, players, n, 270.0);
+        zombies.put(b1.getID(), b1);
+        zfbIds.add(b1.getID());
+
+        ZombieFormationBody b2 = new ZombieFormationBody(z, players, n, 90.0);
+        if (b2.getY() < Window.height - 30) {
+          zombies.put(b2.getID(), b2);
+          zfbIds.add(b2.getID());
+        }
+        n++;
+        yPos -= 20;
+        atCap = yPos < 60;
+      }
     }
 
     zombieFormations.put(z.getID(), zfbIds);
